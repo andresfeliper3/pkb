3c5d6544a39863cd8b5e39c7cfea196774924c4b
andresfeliper3 <andresfeliper.2001@gmail.com>
2025-03-10 17:12:19 -0500

    initial commit

---
+++ src/test/java/com/andresfeliper3/pkb/PkbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andresfeliper3.pkb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PkbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
